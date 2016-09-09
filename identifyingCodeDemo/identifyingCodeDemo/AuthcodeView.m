8f4fb0f1264adc22e7b671fa7c0551f0b294edda
lwg123 <656442711@qq.com>
2016-09-09 14:22:44 +0800

    updata

--- identifyingCodeDemo/identifyingCodeDemo/AuthcodeView.m
+++ identifyingCodeDemo/identifyingCodeDemo/AuthcodeView.m
@@ -32,7 +32,7 @@
     //字符串素材
     _dataArray = [[NSArray alloc] initWithObjects:@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"A",@"B",@"C",@"D",@"E",@"F",@"G",@"H",@"I",@"J",@"K",@"L",@"M",@"N",@"O",@"P",@"Q",@"R",@"S",@"T",@"U",@"V",@"W",@"X",@"Y",@"Z",@"a",@"b",@"c",@"d",@"e",@"f",@"g",@"h",@"i",@"j",@"k",@"l",@"m",@"n",@"o",@"p",@"q",@"r",@"s",@"t",@"u",@"v",@"w",@"x",@"y",@"z",nil];
     _authCodeStr = [[NSMutableString alloc] initWithCapacity:kCharCount];    //随机从数组中选取需要个数的字符串，拼接为验证码字符串
-    for (int i = 0; i < kCharCount; i++)     {
+    for (int i = 0; i < kCharCount; i++) {
         NSInteger index = arc4random() % (_dataArray.count-1);
         NSString *tempStr = [_dataArray objectAtIndex:index];
         _authCodeStr = (NSMutableString *)[_authCodeStr stringByAppendingString:tempStr];
@@ -42,27 +42,37 @@
 #pragma mark 点击界面切换验证码
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    [self getAuthcode];    //setNeedsDisplay调用drawRect方法来实现view的绘制
-    [self setNeedsDisplay];
+    [self getAuthcode];
+    [self setNeedsDisplay];  //setNeedsDisplay调用drawRect方法来实现view的绘制
 }
 
 - (void)drawRect:(CGRect)rect {
-    [super drawRect:rect];    //设置随机背景颜色
-    self.backgroundColor = kRandomColor;    //根据要显示的验证码字符串，根据长度，计算每个字符串显示的位置
+    [super drawRect:rect];
+    
+    self.backgroundColor = kRandomColor;  //设置随机背景颜色
     NSString *text = [NSString stringWithFormat:@"%@",_authCodeStr];
-    CGSize cSize = [@"A" sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20]}];    int width = rect.size.width/text.length - cSize.width;    int height = rect.size.height - cSize.height;    CGPoint point;    //依次绘制每一个字符,可以设置显示的每个字符的字体大小、颜色、样式等
+    //根据要显示的验证码字符串，根据长度，计算每个字符串显示的位置
+    CGSize cSize = [@"A" sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20]}];
+    
+    int width = rect.size.width/text.length - cSize.width;
+    int height = rect.size.height - cSize.height;
+    
+    CGPoint point;
     float pX,pY;
-    for ( int i = 0; i<text.length; i++)     {
+    //依次绘制每一个字符,可以设置显示的每个字符的字体大小、颜色、样式等
+    for ( int i = 0; i<text.length; i++) {
         pX = arc4random() % width + rect.size.width/text.length * i;
         pY = arc4random() % height;
         point = CGPointMake(pX, pY);
         unichar c = [text characterAtIndex:i];
         NSString *textC = [NSString stringWithFormat:@"%C", c];
         [textC drawAtPoint:point withAttributes:@{NSFontAttributeName:kFontSize}];
-    }     //调用drawRect：之前，系统会向栈中压入一个CGContextRef，调用UIGraphicsGetCurrentContext()会取栈顶的CGContextRef
-    CGContextRef context = UIGraphicsGetCurrentContext();    //设置线条宽度
-    CGContextSetLineWidth(context, kLineWidth);    //绘制干扰线
-    for (int i = 0; i < kLineCount; i++)     {
+    }
+    
+    //调用drawRect：之前，系统会向栈中压入一个CGContextRef，调用UIGraphicsGetCurrentContext()会取栈顶的CGContextRef
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineWidth(context, kLineWidth);  //设置线条宽度
+    for (int i = 0; i < kLineCount; i++)     {   //绘制干扰线
         UIColor *color = kRandomColor;
         CGContextSetStrokeColorWithColor(context, color.CGColor);//设置线条填充色
         //设置线的起点
--- identifyingCodeDemo/identifyingCodeDemo/ViewController.m
+++ identifyingCodeDemo/identifyingCodeDemo/ViewController.m
@@ -49,15 +49,16 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     //判断输入的是否为验证图片中显示的验证码
-    if ([_input.text isEqualToString:authCodeView.authCodeStr])     {
+    if ([_input.text isEqualToString:authCodeView.authCodeStr]) {
         //正确弹出警告款提示正确
         UIAlertView *alview = [[UIAlertView alloc] initWithTitle:@"恭喜您 ^o^" message:@"验证成功" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
         [alview show];
     }
-    else {        //验证不匹配，验证码和输入框抖动
+    else {
+        //验证不匹配，验证码和输入框抖动
         CAKeyframeAnimation *anim = [CAKeyframeAnimation animationWithKeyPath:@"transform.translation.x"];
         anim.repeatCount = 1;
-        anim.values = @[@-20,@20,@-20];//        [authCodeView.layer addAnimation:anim forKey:nil];
+        anim.values = @[@-20,@20,@-20];
         [_input.layer addAnimation:anim forKey:nil];
     }
     return YES;
